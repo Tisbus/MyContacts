69f454dcdafc517c59b2b1a9f19be0c7177fc4a7
Tisbus <yog78@mail.ru>
2022-01-10 16:41:34 +0300

    all binding2

--- app/src/main/java/com/example/mycontacts/model/Contact.java
+++ app/src/main/java/com/example/mycontacts/model/Contact.java
@@ -4,8 +4,6 @@ import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
-import java.util.Observable;
-
 @Entity(tableName = "contacts")
 public class Contact{
 
--- app/src/main/java/com/example/mycontacts/model/ContactDataBase.java
+++ app/src/main/java/com/example/mycontacts/model/ContactDataBase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Contact.class}, version = 1, exportSchema = false)
+@Database(entities = {Contact.class}, version = 2, exportSchema = false)
 public abstract class ContactDataBase extends RoomDatabase {
 
     private final static String DB_NAME = "contacts";
